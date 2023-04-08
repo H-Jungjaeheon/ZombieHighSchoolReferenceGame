80a3c598aa43e39ee9c92b16d8440ea19a7a3ba6
H-JungJaeHeon <jjhking222@naver.com>
2023-04-08 22:30:17 +0900

    폰트 추가 및 수정
    폰트 추가 및 수정

---
+++ Assets/Scripts/Client/BattleScene/EnemyData.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyData", menuName = "ScriptableObject/EnemyData", order = int.MaxValue)]
+public class EnemyData : ScriptableObject
+{
+    [SerializeField]
+    [Tooltip("이름")]
+    private string enemyName;
+
+    public string EnemyName
+    {
+        get
+        {
+            return enemyName;
+        }
+    }
+
+    [SerializeField]
+    [Tooltip("체력")]
+    private int hp;
+
+    public int Hp
+    {
+        get
+        {
+            return hp;
+        }
+    }
+
+    [SerializeField]
+    [Tooltip("플레이어 인식 범위")]
+    private float sightRange;
+
+    public float SightRange
+    {
+        get
+        {
+            return sightRange; 
+        }
+    }
+
+    [SerializeField]
+    [Tooltip("이동속도")]
+    private float speed;
+    
+    public float Speed
+    {
+        get 
+        {
+            return speed;
+        }
+    }
+}
