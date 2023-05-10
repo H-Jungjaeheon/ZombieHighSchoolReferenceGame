447195e7d378a69a948738725dba7a172db49bf0
sungbum05 <sblim05@naver.com>
2023-05-10 23:24:24 +0900

    1Day Listener

--- Assets/Networking/CNetworkService.cs
+++ Assets/Networking/CNetworkService.cs
@@ -4,6 +4,8 @@ using System.Threading;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using UnityEngine.Experimental.GlobalIllumination;
+using UnityEngine.UI;
 
 public class CNetworkService
 {
@@ -24,7 +26,103 @@ public class CNetworkService
 
 public class CListener
 {
+    SocketAsyncEventArgs accept_args;
 
+    Socket listen_socket;
+
+    AutoResetEvent flow_control_event;
+
+    public delegate void NewclientHandle(Socket client_socket, object token);
+    public NewclientHandle callback_on_newclient = null;
+
+    public CListener()
+    {
+        this.callback_on_newclient = null;
+    }
+
+    public void start(string host, int port, int backlog)
+    {
+        this.listen_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        IPAddress address;
+        if(host == "0,0,0,0")
+        {
+            address = IPAddress.Any;
+        }
+        else
+        {
+            address = IPAddress.Parse(host);
+        }
+        IPEndPoint endpoint = new IPEndPoint(address, port);
+
+        try
+        {
+            this.listen_socket.Bind(endpoint);
+            this.listen_socket.Listen(backlog);
+
+            this.accept_args = new SocketAsyncEventArgs();
+            this.accept_args.Completed += new EventHandler<SocketAsyncEventArgs>(on_accept_completed);
+
+            this.listen_socket.AcceptAsync(this.accept_args);
+        }
+
+        catch(Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    void do_listen()
+    {
+        this.flow_control_event = new AutoResetEvent(false);
+
+        while(true)
+        {
+            this.accept_args.AcceptSocket = null;
+
+            bool pending = true;
+            try
+            {
+                pending = listen_socket.AcceptAsync(this.accept_args);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                continue;
+            }
+
+            if(!pending)
+            {
+                on_accept_completed(null, this.accept_args);
+            }
+
+            this.flow_control_event.WaitOne();
+        }
+    }
+
+    void on_accept_completed(object sender, SocketAsyncEventArgs e)
+    {
+        if(e.SocketError == SocketError.Success)
+        {
+            Socket client_socket = e.AcceptSocket;
+
+            this.flow_control_event.Set();
+
+            if(this.callback_on_newclient != null)
+            {
+                this.callback_on_newclient(client_socket, e.UserToken);
+            }
+
+            return;
+        }
+        
+        else
+        {
+            Console.WriteLine("Failed to accept client");
+        }
+
+        this.flow_control_event.Set();
+    }
 }
 
 public class SocketAsyncEventArgsPool
